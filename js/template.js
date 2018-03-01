2b7e504bba2fa249318ec231122fb94d88f096ef
Paul C Pederson <paul.c.pederson@gmail.com>
2018-02-28 20:10:29 -0800

    properly working srcset

--- js/template.js
+++ js/template.js
@@ -2,14 +2,14 @@ export default function (listing) {
   var resolutions = listing.preview.images[0].resolutions
   var images = resolutions.map(image => image.url)
   var landscape = resolutions[0].width >= resolutions[0].height
+  var sizes = resolutions.map(image => `${image.url} ${image.width}w`).join(', ')
   return `
     <div class="entry">
-      <picture class="entry--image ${landscape ? "entry--image__landscape" : "entry--image__portrait"}">
-        <source media="(min-width: 320px)" srcset="${images[2]}, ${images[3]} 2x">
-        <source media="(min-width: 640px)" srcset="${images[3]}, ${images[5]} 2x">
-        <source media="(min-width: 960px)" srcset="${images[5]}">
-        <img src="${images[3]}" srcset="${images[4]} 2x" alt="listing">
-      </picture>
+      <img
+        class="entry--image ${landscape ? "entry--image__landscape" : "entry--image__portrait"}"
+        src="${images[3]}" srcset="${sizes}"
+        alt="listing"
+      >
     </div>
   `
 }

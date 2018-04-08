1a6766f89e9612d0cd1acdb28d87d6e2e6cbf87b
Paul C Pederson <paul.c.pederson@gmail.com>
2018-04-08 11:04:36 -0700

    dynamic loading of css

--- js/main.js
+++ js/main.js
@@ -3,28 +3,34 @@ import {getConstraint, inView, $} from './utils/helpers.js'
 import template from './template.js'
 import throttle from 'lodash.throttle'
 
-let ratio = window.innerWidth / window.innerHeight
-let wrapper = document.querySelector('.js-photo-wrap')
+// activate non-critical CSS
+var link = document.querySelector('link[rel="stylesheet"]')
+link.removeAttribute('disabled')
+link.addEventListener('load', function() {
+  document.body.classList.add('loaded')
+})
+
+// fetch reddit images
 let images = []
+let ratio = window.innerWidth / window.innerHeight
 
 rq.get('https://www.reddit.com/r/analog.json')
   .then(render)
   .catch(error => console.error.bind(console))
 
 function render (response) {
-  wrapper.innerHTML = response.data.children
+  document.querySelector('.js-photo-wrap').innerHTML = response.data.children
     .map(listing => listing.data)
     .filter(listing => listing.preview && listing.domain === 'i.redd.it')
     .filter(listing => listing.preview.images[0].resolutions.length > 5)
     .map(listing => template(listing, ratio))
-    .join("")
-  images = $('.js-image')
-    .map(image => {
-      return {
-        node: image,
-        ratio: parseFloat(image.getAttribute('data-ratio'))
-      }
-    })
+    .join('')
+  images = $('.js-image').map(image => {
+    return {
+      node: image,
+      ratio: parseFloat(image.getAttribute('data-ratio'))
+    }
+  })
   lazyLoad()
 }
 
@@ -32,13 +38,16 @@ function updateConstraints (e) {
   ratio = window.innerWidth / window.innerHeight;
   images.forEach(image => {
     let constraint = getConstraint(ratio, image.ratio)
-    image.node.classList = `entry--image ${constraint} js-image`
+    image.node.classList.remove('constrain-x')
+    image.node.classList.remove('constrain-y')
+    image.node.classList.add(constraint)
   })
 }
 
 function fadeIn (el) {
   if (el.target.classList && el.target.classList.contains('js-image')) {
     el.target.parentElement.parentElement.classList.remove('entry__unloaded')
+    document.querySelector('.site-title').classList.add('fade-out')
   }
 }
 

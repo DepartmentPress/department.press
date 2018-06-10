7a72e8e5c9748997532c9c4bdb89da01665d33e0
Nikolas Wise <nikolas@fuzzco.com>
2018-06-10 16:24:20 -0700

    swap out reddit for contentful backend

--- js/main.js
+++ js/main.js
@@ -2,33 +2,39 @@ import rq from './utils/rq.js'
 import {getConstraint, inView, $} from './utils/helpers.js'
 import template from './template.js'
 import throttle from 'lodash.throttle'
+import {createClient} from 'contentful'
 
 // activate non-critical CSS
 document.querySelector('link[rel="stylesheet"]').removeAttribute('disabled')
+
+const ct = createClient({
+  space: 'm56k0gx8kb85',
+  accessToken: 'ffcc0c434ba6d0bd429222c1cadb3190a040bb3d5b460adbd54867e23a28a6a5'
+})
 
 // fetch reddit images
 let images = []
 let ratio = window.innerWidth / window.innerHeight
 
-rq.get('https://www.reddit.com/r/analog.json')
+ct.getEntries()
   .then(render)
-  .catch(error => console.error.bind(console))
+  .catch(console.error)
 
 function render (response) {
-  document.querySelector('.js-photo-wrap').innerHTML = response.data.children
-    .map(listing => listing.data)
-    .filter(listing => listing.preview && listing.domain === 'i.redd.it')
-    .filter(listing => listing.preview.images[0].resolutions.length > 5)
+  console.log(response)
+  document.querySelector('.js-photo-wrap').innerHTML = response.items
+    .map(listing => listing.fields)
     .map(listing => template(listing, ratio))
     .join('')
-  images = $('.js-image').map(image => {
-    return {
-      node: image,
-      ratio: parseFloat(image.getAttribute('data-ratio'))
-    }
-  })
+  // images = $('.js-image').map(image => {
+  //   return {
+  //     node: image,
+  //     ratio: parseFloat(image.getAttribute('data-ratio'))
+  //   }
+  // })
   lazyLoad()
 }
+
 
 function updateConstraints (e) {
   ratio = window.innerWidth / window.innerHeight;
@@ -54,8 +60,8 @@ function lazyLoad () {
     .filter(inView)
     .forEach(el => {
       var image = el.querySelector('.js-image')
-      image.srcset = image.getAttribute("data-srcset")
-      image.src = image.getAttribute("data-src")
+      // image.srcset = image.getAttribute("data-srcset")
+      // image.src = image.getAttribute("data-src")
       image.addEventListener("load", fadeIn)
     })
 }
--- js/template.js
+++ js/template.js
@@ -1,25 +1,28 @@
-import {getConstraint} from './utils/helpers'
-
 export default function (listing, screenRatio) {
-  let resolutions = listing.preview.images[0].resolutions
-  let images = resolutions.map(image => image.url)
-  let imageRatio = resolutions[0].width / resolutions[0].height
-  let constrain = getConstraint(screenRatio, imageRatio)
-  let sizes = resolutions.map(image => `${image.url} ${image.width}w`).join(', ')
+  let images = listing.images
+    .map(image => image.fields)
   return `
     <figure class="entry entry__unloaded js-entry">
       <div class="entry--content">
         <img
-          class="entry--image ${constrain} js-image"
-          data-ratio="${imageRatio}"
-          data-src="${images[3]}"
-          data-srcset="${sizes}"
+          class="entry--image constrain-${listing.constrain} js-image"
+          src="${images[0].file.url}"
           alt="${listing.title}"
         >
         <figcaption class="entry--caption">
-          <a class="entry--link" href="https://reddit.com${listing.permalink}" _target="blank">${listing.title}</a>
+          <em>${listing.title}</em>, ${listing.year}
+          <br />${listing.description} Edition of ${listing.editionSize}
         </figcaption>
       </div>
     </figure>
   `
 }
+
+// let resolutions = listing.images[0].resolutions
+// let images = resolutions.map(image => image.url)
+// let imageRatio = resolutions[0].width / resolutions[0].height
+// let constrain = getConstraint(screenRatio, imageRatio)
+// let sizes = resolutions.map(image => `${image.url} ${image.width}w`).join(', ')
+// data-ratio="${imageRatio}"
+// data-src="${images[0]}"
+// data-srcset="${sizes}"
